b92644b6b31e49170e2545e2414dbbef8873db52
leesangyoon <pinetree408@gmail.com>
2015-07-29 18:04:05 +0900

    add facebook login

--- emotiondiary/members/views.py
+++ emotiondiary/members/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from allauth.socialaccount.models import SocialAccount
 
-# Create your views here.
+def index(request):
+
+	print request.user.id
+	user = SocialAccount.objects.filter(user_id=request.user.id, provider='facebook')
+
+	context={'test': user[0].extra_data}
+
+	return render(request, 'index.html', context)
+
+
