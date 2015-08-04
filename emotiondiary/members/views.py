dcbec29c349ec8dd3e634aaaa3533ba1e8287c16
leesangyoon <pinetree408@gmail.com>
2015-08-04 13:48:09 +0900

    add user profile image

--- emotiondiary/members/views.py
+++ emotiondiary/members/views.py
@@ -9,8 +9,11 @@ class SocialAccountDetailMixin(object):
 		if request.user.is_authenticated():
 			user = SocialAccount.objects.filter(user_id=request.user.id, provider='facebook')
 			user_profile = user[0].extra_data
+
 		
 		context = super(SocialAccountDetailMixin, self).get_context_data(**kwargs)
-		context['test'] = user_profile
+		context['profile'] = user_profile
+		print user_profile
+		context['profile_image'] = "http://graph.facebook.com/{}/picture?width=40&height=40".format(user_profile['id'])
 		return context
 
