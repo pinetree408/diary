f79fda5e001ea948308cc8ff9cc49213db6073ac
leesangyoon <pinetree408@gmail.com>
2015-08-10 17:04:43 +0900

    change README & fix facebook login at member

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ An application for evaluating emotions on Facebook.
 ## How to Work on this Project (on a mac)
 
 ### Setting up your system
-```
+```sh
 git clone https://github.com/pinetree408/emotiondiary.git
 
 cd emotiondiary
@@ -17,7 +17,16 @@ virtualenv venv
 . venv/bin/activate 
 
 pip install -r requirements.txt
+
+cd emotiondiary
+
+python manage.py migrate
+
+python manage.py createsuperuser
 ```
+Add Site facebook.com at admin page
+
+Add facebook application at Social applications page
 
 ### Running on local
 ```
--- emotiondiary/members/views.py
+++ emotiondiary/members/views.py
@@ -5,15 +5,17 @@ class SocialAccountDetailMixin(object):
 
 	def get_context_data(self,  **kwargs):
 		user_profile = ''
+		user_profile_image = ''
+
+		context = super(SocialAccountDetailMixin, self).get_context_data(**kwargs)
+
 		request = self.request
 		if request.user.is_authenticated():
 			user = SocialAccount.objects.filter(user_id=request.user.id, provider='facebook')
 			user_profile = user[0].extra_data
+			user_profile_image = "http://graph.facebook.com/{}/picture?width=40&height=40".format(user_profile['id'])
 
-		
-		context = super(SocialAccountDetailMixin, self).get_context_data(**kwargs)
 		context['profile'] = user_profile
-		print user_profile
-		context['profile_image'] = "http://graph.facebook.com/{}/picture?width=40&height=40".format(user_profile['id'])
-		return context
+		context['profile_image'] = user_profile_image
 
+		return context
