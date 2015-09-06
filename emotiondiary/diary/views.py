02328fa52e38770f26784b73fb9a34033acdbacb
leesangyoon <pinetree408@gmail.com>
2015-09-06 16:10:40 +0900

    Divide Diary to Diary & DiaryCreate

--- emotiondiary/diary/views.py
+++ emotiondiary/diary/views.py
@@ -1,15 +1,39 @@
-from django.views.generic import CreateView
+from django.views.generic import ListView, CreateView
 from allauth.socialaccount.models import SocialAccount
 from diary.forms import MessageForm
 from diary.models import Message
+from django.shortcuts import redirect
 
 import json
 
-class DiaryView(CreateView):
+class DiaryView(ListView):
 	template_name = "diary/diary.html"
+	model = Message
+
+	def dispatch(self, request, *args, **kwargs):
+		user = SocialAccount.objects.filter(user_id=self.request.user.id, provider='facebook')
+		messages_object = Message.objects.filter(user=user)
+		if (messages_object.count() == 0):
+			return redirect('/diary/create')
+
+        	return super(DiaryView, self).dispatch(request, *args, **kwargs)
+
+
+	def get_context_data(self,  **kwargs):
+
+		context = super(DiaryView, self).get_context_data(**kwargs)
+		user = SocialAccount.objects.filter(user_id=self.request.user.id, provider='facebook')
+		messages_object = Message.objects.filter(user=user)		
+		messages = list(map(lambda message: message.as_dict(), messages_object))
+		
+		context['messages'] = messages
+
+		return context
+
+class DiaryCreateView(CreateView):
+	template_name = "diary/diary-create.html"
 	form_class = MessageForm
 	success_url = '/diary'
-	model = Message
 
 	def form_valid(self, form):
 		user = SocialAccount.objects.filter(user_id=self.request.user.id, provider='facebook')
@@ -17,14 +41,4 @@ class DiaryView(CreateView):
 		self.object.user = user[0]
 		self.object.save();
 
-		return super(DiaryView, self).form_valid(form)
-
-	def get_context_data(self,  **kwargs):
-
-		context = super(DiaryView, self).get_context_data(**kwargs)
-		user = SocialAccount.objects.filter(user_id=self.request.user.id, provider='facebook')
-		messages = Message.objects.filter(user=user)		
-		messages = list(map(lambda message: message.as_dict(), messages))
-		context['messages'] = messages
-
-		return context
+		return super(DiaryCreateView, self).form_valid(form)
--- emotiondiary/emotiondiary/urls.py
+++ emotiondiary/emotiondiary/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from app.views import IndexView
-from diary.views import DiaryView 
+from diary.views import DiaryView, DiaryCreateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^$', IndexView.as_view()),
     url(r'^diary$', DiaryView.as_view()),
+    url(r'^diary/create$', DiaryCreateView.as_view()),
 ]
