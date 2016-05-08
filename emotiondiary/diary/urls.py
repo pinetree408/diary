4d9fe4b7223530235ca7c50f02abc6d9c6cd5a9b
leesangyoon <pinetree408@gmail.com>
2016-05-08 18:13:48 +0900

    Add login required to diary create view

--- emotiondiary/diary/urls.py
+++ emotiondiary/diary/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from diary import views
 
 urlpatterns = [
     url(r'^$', views.DiaryView.as_view(), name='index'),
-    url(r'^create/$', views.DiaryCreateView.as_view(), name='create'),
+    url(r'^create/$', login_required(views.DiaryCreateView.as_view()), name='create'),
 ]
