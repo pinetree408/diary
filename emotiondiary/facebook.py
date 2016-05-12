d36f216b1f62e8eea21701222839f85ee41ad0f2
leesangyoon <pinetree408@gmail.com>
2016-05-12 22:07:51 +0900

    Remove facebook app client & secret key

--- emotiondiary/facebook.py
+++ emotiondiary/facebook.py
@@ -6,9 +6,9 @@ facebook_site = Site.objects.create(domain='facebook.com', name='facebook.com')
 
 social_app = SocialApp.objects.create(
     provider='facebook',
-    name='emotiondiary',
-    client_id='1447094345617583',
-    secret='54e60cc48404307a1ccbcee04bb4a04c')
+    name='',
+    client_id='',
+    secret='')
 
 social_app.save()
 social_app.sites.add(example_site)
