1a15964e447cd1acc88dae81cae07c707c6cd60d
leesangyoon <pinetree408@gmail.com>
2016-08-30 11:36:26 +0900

    Add function which checks auth for diary view

--- emotiondiary/diary/urls.py
+++ emotiondiary/diary/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from diary import views
 
 urlpatterns = [
-    url(r'^$', views.DiaryView.as_view(), name='index'),
+    url(r'^$', login_required(views.DiaryView.as_view()), name='index'),
     url(r'^create/(?P<year>\d+)/(?P<month>\d+)/(?P<date>\d+)/$', login_required(views.DiaryCreateView.as_view()), name='create'),
     url(r'^update/(?P<pk>\d+)/$', login_required(views.DiaryUpdateView.as_view()), name='update'),
 ]
